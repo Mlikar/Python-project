b1c16b97ac8f94c726475783a145c83ef38c7660
Mlikar <115452874+Mlikar@users.noreply.github.com>
2023-02-22 14:59:00 +0100

    Update Function.py

--- Kalkulačka/Function.py
+++ Kalkulačka/Function.py
@@ -3,52 +3,52 @@ import math
 import Basic
 
 # Funkce
-# zde používáme knihovnu math pro matematické úlohy
-# tyto funkce fungují podobně jako funkce calculate, při použit vyhodí rovnou výsledek, avšak počítají pouze s jedním číslem
+# Zde používáme knihovnu math pro matematické úlohy
+# Tyto funkce fungují podobně jako funkce calculate, při použit vyhodí rovnou výsledek, avšak počítají pouze s jedním číslem
 
-# funkce pro odmocninu 
+# Funkce pro odmocninu 
 def odmocnina(field):
     sqrt = math.sqrt(float(Basic.field_text))
     field.delete("1.0", "end")
     field.insert("1.0", sqrt)
     field_text = str(sqrt)
 
-# funkce pro mocninu
+# Funkce pro mocninu
 def mocnina(field):
     pow = math.pow(float(Basic.field_text), 2)
     field.delete("1.0", "end")
     field.insert("1.0", pow)
     field_text = str(pow)
 
-# funkce pro faktorial
+# Funkce pro faktorial
 def faktorial(field):
     factorial = math.factorial(int(Basic.field_text))
     field.delete("1.0", "end")
     field.insert("1.0", factorial)
     field_text = str(factorial)
 
-# funkce pro sinus
+# Funkce pro sinus
 def sin(field):
     sin = math.sin(float(Basic.field_text))
     field.delete("1.0", "end")
     field.insert("1.0", sin)
     field_text = str(sin)
 
-# funkce pro cosinus
+# Funkce pro cosinus
 def cos(field):
     cos = math.cos(float(Basic.field_text))
     field.delete("1.0", "end")
     field.insert("1.0", cos)
     field_text = str(cos)
 
-# funkce pro tangenc
+# Funkce pro tangenc
 def tg(field):
     tg = math.tan(float(Basic.field_text))
     field.delete("1.0", "end")
     field.insert("1.0", tg)
     field_text = str(tg)
 
-# funkce pro cotangenc
+# Funkce pro cotangenc
 def cotg(field):
     cotg = math.cos(int(Basic.field_text)) / math.sin(int(Basic.field_text))
     field.delete("1.0", "end")
