c238a99ddbcf15fceaddf13cca6d030b894b3c5a
Mlikar <115452874+Mlikar@users.noreply.github.com>
2023-02-22 14:58:30 +0100

    Update Window.py

--- Kalkulačka/Window.py
+++ Kalkulačka/Window.py
@@ -1,14 +1,14 @@
-import tkinter as tk
-import keyboard
+import tkinter as tk  # knihovna pro tvoření okna, tlačítek a pole
+import keyboard # knihovna pro použití klávesnice
 from tkinter import*
 from Basic import *
 from Function import *
 
-nahrada = 0
 
  
 # Window
-
+# Tvoření samotného okna, "geometry" slouží k celkové velikost okna
+# Dále tvoření field, neboli pole kde budeme vkládat čísla, znaménka a provádět výpočty
 window = Tk()
 window.geometry("400x250")
 field = tk.Text(window, height= 2, width=21, font=("Times new roman", 20))
@@ -17,6 +17,8 @@ field.grid(row=1, column=1, columnspan=7)
 
 
 # Button commands
+# Zde tvoříme jednotlivá tlačítka, zároveň jim udaváme co mají v sobě napsané, font a hlavně jejich funkci
+# Jednotlivá čísla
 button_1 = tk.Button(window, text="1", command = lambda: add_to_field(1, field), width=3, font=("Times new roman", 15))
 button_2 = tk.Button(window, text="2", command = lambda: add_to_field(2, field), width=3, font=("Times new roman", 15))
 button_3 = tk.Button(window, text="3", command = lambda: add_to_field(3, field), width=3, font=("Times new roman", 15))
@@ -28,30 +30,34 @@ button_8 = tk.Button(window, text="8", command = lambda: add_to_field(8, field),
 button_9 = tk.Button(window, text="9", command = lambda: add_to_field(9, field), width=3, font=("Times new roman", 15))
 button_0 = tk.Button(window, text="0", command = lambda: add_to_field(0, field), width=3, font=("Times new roman", 15))
 
-
+# Základní znaménka
 button_plus = tk.Button(window, text="+", command = lambda: add_to_field("+", field), width=3, font=("Times new roman", 15))
 button_minus = tk.Button(window, text="-", command = lambda: add_to_field("-", field), width=3, font=("Times new roman", 15))
 button_krat = tk.Button(window, text="*", command = lambda: add_to_field( "*", field), width=3, font=("Times new roman", 15))
 button_deleno = tk.Button(window, text="/", command = lambda: add_to_field( "/", field), width=3, font=("Times new roman", 15))
 
+# Závorky a desetinna
 button_lzavor = tk.Button(window, text="(", command=lambda: add_to_field("(", field), width=3, font=("Times new roman", 15))
 button_pzavor = tk.Button(window, text=")", command=lambda: add_to_field(")", field), width=3, font=("Times new roman", 15))
 button_desetinna = tk.Button(window, text=".", command = lambda: add_to_field(".", field), width=3, font=("Times new roman", 15))
 
+# Pkročilé matmatické funkce
 button_faktorial = tk.Button(window, text="!", command = lambda: faktorial(field), width=3, font=("Times new roman", 15))
 button_odmocnina = tk.Button(window, text="√", command=lambda: odmocnina(field), width=3, font=("Times new roman", 15))
 button_mocnina = tk.Button(window, text="x²", command=lambda: mocnina(field), width=3, font=("Times new roman", 15))
 
+# Goniometrické funkce
 button_sin = tk.Button(window, text="sin", command=lambda: sin(field), width=3, font=("Times new roman", 15))
 button_cos = tk.Button(window, text="cos", command=lambda: cos(field), width=3, font=("Times new roman", 15))
 button_tg = tk.Button(window, text="tg", command=lambda: tg(field), width=3, font=("Times new roman", 15))
 button_cotg = tk.Button(window, text="cotg", command=lambda: cotg(field), width=3, font=("Times new roman", 15))
 
+# Výpočet a čištění pole
 button_clear = tk.Button(window, text="Clear", command=lambda: clear(field), width=7, font=("Times new roman", 15))
 button_rovnase = tk.Button(window, text="=", command=lambda: calculate(field), width=7, font=("Times new roman", 15))
 
 
-# umístění jednotlivých tlačítek do okna
+# Umístění jednotlivých tlačítek do okna
 button_1.grid(row = 4, column= 1)
 button_2.grid(row = 4, column= 2)
 button_3.grid(row = 4, column= 3)
@@ -90,10 +96,9 @@ button_clear.grid(row = 7, column= 4, columnspan=2)
 ### Key press ###
 #################
 
-# funkce bind a knihovna keyboard, pro počítání pomocí klávesnice
+# Zde požíváme funkci bind a knihovnu keyboard, abychom mohli použít klávesnici k použití naší kalkulačky
 
 ### Cisla ###
-
 window.bind('1', lambda event: add_to_field(1, field))
 window.bind('2', lambda event: add_to_field(2, field))
 window.bind('3', lambda event: add_to_field(3, field))
@@ -114,7 +119,6 @@ window.bind('/', lambda event: add_to_field("/", field))
 ### Funkce ###
 window.bind(',', lambda event: add_to_field(".", field))
 window.bind('.', lambda event: add_to_field(".", field))
-
 
 keyboard.add_hotkey('Enter', lambda: calculate(field))
 keyboard.add_hotkey('Backspace', lambda: clear(field))
