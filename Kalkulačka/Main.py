eebe806f76141a5a5ae479acd3e017e28ae41379
Mlikar <115452874+Mlikar@users.noreply.github.com>
2023-02-27 16:53:59 +0100

    Update Main.py

--- Kalkulačka/Main.py
+++ Kalkulačka/Main.py
@@ -1,6 +1,10 @@
 import Window
 from Window import*
 
+"""
+Zde vkládáme soubor "Window.py" a spouštíme celý program, pomocí funkce "Main".
+"""
+
 # Zde voláme funkci z "Window" abychom zapnuli celý program
 def Main():
     Window.window(mainloop) 
