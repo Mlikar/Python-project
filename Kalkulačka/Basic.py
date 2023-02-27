7a58a8b4f8c5c854da5aa53a9d433d83a6e4097f
Mlikar <115452874+Mlikar@users.noreply.github.com>
2023-02-27 16:55:21 +0100

    Update Basic.py

--- Kalkulačka/Basic.py
+++ Kalkulačka/Basic.py
@@ -1,6 +1,16 @@
 
+"""
+Basic.py
+Zde se náchází první tři základní funkce naší kalkulačky a základní proměnná, která slouží k
+zapisování do pole(field).
+"""
 
 field_text=""  # Proměná do které, vkládáme čísla a znaménka, které chceme v poli
+
+"""
+Funkce add_to_field
+Zapisuje do pole číslo nebo základní znaménko.
+"""
 
 # Tato funkce slouží k přidávání čísel a znamének do pole (field)
 def add_to_field(add, field):
@@ -9,6 +19,11 @@ def add_to_field(add, field):
     field.delete("1.0", "end")
     field.insert("1.0", field_text)
 
+"""
+Funkce calculate
+Vezme čísla a základní znaménka z pole, vloží je do proměnné "result" a výsledek zobrazí v poli
+Pak proměnnou "result", vkládáme do proměnné "field_text," abychom mohli s výsledkem dále počítat.
+"""
 
 # Funkce pro finální výpočet příkladu
 # Proměná result slouží k tomu, abychom pak s výsledkem mohli počítat dále 
@@ -19,6 +34,11 @@ def calculate(field):
     field.insert("1.0", result)
     field_text = result
 
+"""
+Funkce clear
+Tato funkce vyčistí pole a vymaže data z proměnných.
+""" 
+    
  # Funkce pro vyčištění pole a proměných
 def clear(field):
     global field_text
